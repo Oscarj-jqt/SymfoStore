76ac8e85694d6bd655673d223ed1e3b28e70a3f6
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-02-28 14:28:38 +0100

    config product controller

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Category;
 use App\Entity\Product;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
@@ -22,18 +21,16 @@ final class ProductController extends AbstractController
     public function index(ProductRepository $productRepository, SerializerInterface $serializer): Response
     {
         $products = $productRepository->findAll();
-
-        $json = $serializer->serialize($products, 'json', ['groups' => ['product:read']]);
-
+        $json = $serializer->serialize($products, 'json', ['groups' => 'product:read']);
         return new JsonResponse($json, Response::HTTP_OK, [], true);
     }
 
-    #[Route('/new/{id}', name: 'app_product_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'app_product_new', methods: ['POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager, CategoryRepository $categoryRepository, SerializerInterface $serializer, ValidatorInterface $validator): Response
     {
         $data = json_decode($request->getContent(), true);
 
-        if (!$data || !isset($data['name']) || !isset($data['category_id'])) {
+        if (!$data || !isset($data['name'], $data['category_id'], $data['description'], $data['price'])) {
             return $this->json(['error' => 'Invalid data'], Response::HTTP_BAD_REQUEST);
         }
 
@@ -43,13 +40,10 @@ final class ProductController extends AbstractController
         }
 
         $product = new Product();
-        $product->setName($data['name']);
-        //        $product->setCategory($category['category_id']);
-        $product->setDescription($data['description']);
-        $product->setPrice($data['price']);
-        $product->setCategory($category);
-
-        $category = $entityManager->getRepository(Category::class)->find($data['category_id']);
+        $product->setName($data['name'])
+            ->setDescription($data['description'])
+            ->setPrice($data['price'])
+            ->setCategory($category);
 
         $errors = $validator->validate($product);
         if (count($errors) > 0) {
@@ -57,34 +51,29 @@ final class ProductController extends AbstractController
             foreach ($errors as $error) {
                 $errorMessages[] = $error->getMessage();
             }
-
             return $this->json(['errors' => $errorMessages], Response::HTTP_BAD_REQUEST);
         }
 
         $entityManager->persist($product);
         $entityManager->flush();
 
-        $json = $serializer->serialize($product, 'json', ['groups' => ['product:read']]);
-
-        return new JsonResponse($json, 200, [], true);
+        $json = $serializer->serialize($product, 'json', ['groups' => 'product:read']);
+        return new JsonResponse($json, Response::HTTP_CREATED, [], true);
     }
 
     #[Route('/show/{id}', name: 'app_product_show', methods: ['GET'])]
     public function show(Product $product, SerializerInterface $serializer): JsonResponse
     {
-        $json = $serializer->serialize($product, 'json', ['groups' => ['product:read']]);
-
+        $json = $serializer->serialize($product, 'json', ['groups' => 'product:read']);
         return new JsonResponse($json, Response::HTTP_OK, [], true);
     }
 
-    #[Route('/edit/{id}', name: 'app_product_edit', methods: ['GET', 'PUT'])]
-    public function edit(Request $request, Product $product, EntityManagerInterface $entityManager, CategoryRepository $categoryRepository, ValidatorInterface $validator): Response
+    #[Route('/edit/{id}', name: 'app_product_edit', methods: ['PUT'])]
+    public function edit(Request $request, Product $product, EntityManagerInterface $entityManager, CategoryRepository $categoryRepository, ValidatorInterface $validator, SerializerInterface $serializer): Response
     {
-        //        checking if role is admin
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
         $data = json_decode($request->getContent(), true);
-
         if (!$data) {
             return $this->json(['error' => 'Invalid data'], Response::HTTP_BAD_REQUEST);
         }
@@ -92,14 +81,12 @@ final class ProductController extends AbstractController
         if (isset($data['name'])) {
             $product->setName($data['name']);
         }
-
         if (isset($data['description'])) {
             $product->setDescription($data['description']);
         }
         if (isset($data['price'])) {
             $product->setPrice($data['price']);
         }
-
         if (isset($data['category_id'])) {
             $category = $categoryRepository->find($data['category_id']);
             if (!$category) {
@@ -114,23 +101,18 @@ final class ProductController extends AbstractController
             foreach ($errors as $error) {
                 $errorMessages[] = $error->getPropertyPath().' : '.$error->getMessage();
             }
-
             return $this->json(['errors' => $errorMessages], Response::HTTP_BAD_REQUEST);
         }
 
         $entityManager->flush();
-
-        return $this->json($product, ['message' => 'Product updated successfully'], Response::HTTP_OK);
+        $json = $serializer->serialize($product, 'json', ['groups' => 'product:read']);
+        return new JsonResponse($json, Response::HTTP_OK, [], true);
     }
 
     #[Route('/delete/{id}', name: 'app_product_delete', methods: ['DELETE'])]
-    public function delete(Request $request, Product $product, EntityManagerInterface $entityManager): Response
+    public function delete(Product $product, EntityManagerInterface $entityManager): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
-
-        if (!$product) {
-            return $this->json(['error' => 'Product not found'], Response::HTTP_NOT_FOUND);
-        }
 
         $entityManager->remove($product);
         $entityManager->flush();
