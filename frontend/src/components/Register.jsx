8df1d2ea723d6389e543efa6ba87d9c57ca52fb0
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-03-11 16:28:31 +0100

    Add register component

---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import ProductsList from "./ProductsList";
+
+const Register = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+    const [isRegistered, setIsRegistered] = useState(false);
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch("http://127.0.0.1:8000/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ email, password }),
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                localStorage.setItem("token", data.token);
+                localStorage.setItem("role", data.roles);
+                setMessage("Inscription réussie !");
+                setIsRegistered(true);
+            } else {
+                setMessage("Erreur : " + (data.error || "Problème lors de l'inscription"));
+            }
+        } catch (error) {
+            setMessage("Erreur de connexion au serveur.");
+        }
+    };
+
+    return (
+        <div>
+            {!isRegistered ? (
+                <>
+                    <h2>Inscription</h2>
+                    <form onSubmit={handleRegister}>
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <input
+                            type="password"
+                            placeholder="Mot de passe"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <button type="submit">S'inscrire</button>
+                    </form>
+                    <p>{message}</p>
+                </>
+            ) : (
+                <>
+                    <p style={{ color: "green", fontWeight: "bold" }}>{message}</p>
+                    <ProductsList />
+                </>
+            )}
+        </div>
+    );
+};
+
+export default Register;
